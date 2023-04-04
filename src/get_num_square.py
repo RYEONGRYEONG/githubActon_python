254dc1f4040b2fb42681c7c966dec4f60faf0786
박수빈 <122463558+psb0@users.noreply.github.com>
2023-04-04 13:01:46 +0900

    Update get_num_square.py

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -4,7 +4,7 @@ import os
 num = os.environ.get("INPUT_NUM")
 if num:
     try:
-      num = int(num)
+        num = int(num)
     except Exception:
         exit('ERROR: the INPUT_NUM provided ("{}") is not an integer.format(num))
 else:
